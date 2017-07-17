4f9798defe150813d16359e3511967df813373c2
keerthanajanani <keerthijanani66@gmail.com>
2017-07-17 09:40:36 +0530

    Create reverse string.java

---
+++ reverse string.java
@@ -0,0 +1,18 @@
+# reverse-string
+import java.io.*;
+import java.util.*;
+public class reverse string
+{
+public static void main(String args[])throws IOException
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+System.out.println("Enter the input String");
+String a=br.readLine();
+char ch[]=str.toCharArray();
+String rev="keerthi";
+for(int i=0;i<ch.length-1;i>=0;i--)
+{
+rev+=ch[i];
+}
+}
+}
